07786362b07b6df80c7f20ef5e385425785989e2
ermu <168235638@qq.com>
2024-12-09 19:31:51 +0800

    ermu-修改Header，Aside

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -7,22 +7,18 @@
         default-active="2"
         class="el-menu-vertical-demo">
 
-      <el-sub-menu index="1-4">
-        <template #title>选项 4</template>
-        <el-menu-item index="1-4-1">选项 1</el-menu-item>
+      <el-sub-menu index="1">
+        <template #title>信息管理</template>
+        <el-menu-item index="1-1">图书管理</el-menu-item>
+        <el-menu-item index="1-2">通知管理</el-menu-item>
+        <el-menu-item index="1-3">借阅管理</el-menu-item>
+        <el-menu-item index="1-3">预约管理</el-menu-item>
       </el-sub-menu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <template #title>导航二</template>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <template #title>导航三</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <template #title>导航四</template>
-      </el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>用户管理</template>
+        <el-menu-item index="2-1">管理员信息</el-menu-item>
+        <el-menu-item index="2-2">用户信息</el-menu-item>
+      </el-sub-menu>
     </el-menu>
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,25 @@
 <template>
   <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display:
 flex">
-    <div style="width: 200px; padding-left: 30px; font-weight: bold; color: dodgerblue">后台管理</div>
+    <div style="width: 200px; padding-left: 30px; font-weight: bold; color: dodgerblue">图书管理系统</div>
     <div style="flex: 1"></div>
-    <div style="width: 100px">
+    <div style="width: 150px; display: flex; justify-content: center; align-items: center; font-size: 16px;">
+      欢迎：
       <el-dropdown>
         <span class="el-dropdown-link">
-          tom<i class="el-icon-arrow-right"></i>
+
+          tom
+          <!--<i class="el-icon-arrow-right"></i>-->
         </span>
         <template #dropdown>
           <el-dropdown-menu>
             <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>修改密码</el-dropdown-item>
             <el-dropdown-item>退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
+
 
     </div>
   </div>
