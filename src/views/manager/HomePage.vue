c4c13bf9ba9acb3b9bcb1e348b3769d98bace010
ywqtttu <1973614480@qq.com>
2024-12-25 21:21:37 +0800

    24.12.25.21.21 修改管理员主页大图

--- src/views/manager/HomePage.vue
+++ src/views/manager/HomePage.vue
@@ -37,6 +37,17 @@ export default {
       // 初始化折线图
       const lineChart = echarts.init(this.$refs.lineChart);
       const lineOption = {
+        title: {
+          text: '最近一周借阅数据', // 设置标题文本
+          subtext: '数据来源：内部统计', // 可选的副标题
+          left: 'center', // 标题居中
+          top: '10', // 标题距离顶部的距离
+          textStyle: {
+            fontSize: 20, // 标题字体大小
+            fontWeight: 'bold', // 标题字体粗细
+            color: '#333', // 标题文字颜色
+          }
+        },
         xAxis: {
           type: 'category',
           data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
@@ -46,7 +57,8 @@ export default {
         },
         series: [
           {
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            name: "近一周借阅量",
+            data: [82.0, 93.2, 90.1, 93.4, 129.0, 133.0, 132.0],
             type: 'line',
             smooth: true,
           }
@@ -57,16 +69,27 @@ export default {
       // 初始化柱状图
       const barChart = echarts.init(this.$refs.barChart);
       const barOption = {
+        title: {
+          text: '最近一周归还数据', // 设置标题文本
+          subtext: '数据来源：内部统计', // 可选的副标题
+          left: 'center', // 标题居中
+          top: '10', // 标题距离顶部的距离
+          textStyle: {
+            fontSize: 20, // 标题字体大小
+            fontWeight: 'bold', // 标题字体粗细
+            color: '#333', // 标题文字颜色
+          }
+        },
         xAxis: {
           type: 'category',
-          data: ['A', 'B', 'C', 'D', 'E'],
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         },
         yAxis: {
           type: 'value',
         },
         series: [
           {
-            data: [150, 232, 201, 154, 190],
+            data: [61, 42, 51, 43, 61, 72, 83],
             type: 'bar',
           }
         ]
@@ -76,17 +99,28 @@ export default {
       // 初始化饼图
       const pieChart = echarts.init(this.$refs.pieChart);
       const pieOption = {
+        title: {
+          text: '最近一年最热图书分类', // 设置标题文本
+          subtext: '数据来源：内部统计', // 可选的副标题
+          left: 'center', // 标题居中
+          top: '10', // 标题距离顶部的距离
+          textStyle: {
+            fontSize: 20, // 标题字体大小
+            fontWeight: 'bold', // 标题字体粗细
+            color: '#333', // 标题文字颜色
+          }
+        },
         series: [
           {
-            name: '访问来源',
+            name: '出借图书种类分布图',
             type: 'pie',
             radius: '50%',
             data: [
-              { value: 335, name: '直接访问' },
-              { value: 310, name: '邮件营销' },
-              { value: 234, name: '联盟广告' },
-              { value: 135, name: '视频广告' },
-              { value: 1548, name: '搜索引擎' }
+              { value: 335, name: '教材' },
+              { value: 310, name: '艺术' },
+              { value: 234, name: '生活' },
+              { value: 135, name: '其它' },
+              { value: 1548, name: '文学' }
             ],
             emphasis: {
               itemStyle: {
